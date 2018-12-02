e7461189edf97a9a62eda8c3716343d43f2c921a
OUVRY Thomas <ouvrythomas@gmail.com>
2018-12-02 17:44:59 +0100

    Ligne js

--- js/premierJS.js
+++ js/premierJS.js
@@ -1,22 +1,54 @@
 var canvas = document.getElementById("myCanvas");
+canvas.width = innerWidth;
+canvas.height = innerHeight;
 var context = canvas.getContext("2d");
-draw();
+let nbCol = 8;
+let nbLigne = 4;
+
+
 cercles();
 
 function draw() {
-  var longueurCanvas = canvas.width;
-  var hauteurCanvas = canvas.height;
-
-  for (var i = 200; i < longueurCanvas; i += 125) {
+  context.strokeStyle = "white";
+  // Horizontaux
+  var xHorizon = 400;
+  let yHorizon = 150;
+  for (var i = 1; i <= nbCol; i++) {
     context.beginPath();
-    context.arc(i, 75, 50, 0, 2 * Math.PI);
+    context.arc(xHorizon, yHorizon, 75, 0, 2 * Math.PI);
     context.stroke();
+    xHorizon += 250;
   }
-  for (var i = 200; i < hauteurCanvas; i += 125) {
+  // Verticaux
+  let xVertical = 150;
+  var yVertical = 350;
+  for (var i = 1; i <= nbLigne; i++) {
     context.beginPath();
-    context.arc(75, i, 50, 0, 2 * Math.PI);
+    context.arc(xVertical, yVertical, 75, 0, 2 * Math.PI);
     context.stroke();
+    yVertical += 250;
   }
 }
 
-function cercles() {}
+function cercles() {
+  requestAnimationFrame(cercles);
+  context.clearRect(0, 0, innerWidth, innerHeight);
+  draw();
+  var x = 400;
+  var y = 350;
+  for (var i = 1; i <= nbCol; i++) {
+    context.beginPath();
+    context.moveTo(x, 0);
+    context.lineTo(x, innerHeight);
+    context.stroke();
+    x += 250;
+  }
+  for (var i = 1; i <= nbLigne; i++) {
+    context.beginPath();
+    context.moveTo(0, y);
+    context.lineTo(innerWidth, y);
+    context.stroke();
+    y += 250;
+  }
+
+}
