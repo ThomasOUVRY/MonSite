ae1a4cae4cbcd1f50fd7faab9320e6148731ffb9
ouvryt <ouvrythomas@gmail.com>
2018-12-05 20:48:52 +0100

    refactoring code js

--- js/premierJS.js
+++ js/premierJS.js
@@ -2,53 +2,71 @@ var canvas = document.getElementById("myCanvas");
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 var context = canvas.getContext("2d");
-let nbCol = 8;
-let nbLigne = 4;
-
+const NB_CERCLES = 5;
+const DECALAGE_HORIZON = innerWidth * 0.35;
+const DECALAGE_VERTICAL = innerHeight * 0.35;
+const RAYON = 75;
 
 cercles();
+courbes();
 
 function draw() {
   context.strokeStyle = "white";
-  // Horizontaux
-  var xHorizon = 400;
   let yHorizon = 150;
-  for (var i = 1; i <= nbCol; i++) {
+  let xVertical = DECALAGE_HORIZON - 250;
+  for (var i = 0; i < NB_CERCLES; i++) {
+    // Cercles 1ère ligne
+    var x = DECALAGE_HORIZON + 250 * i;
     context.beginPath();
-    context.arc(xHorizon, yHorizon, 75, 0, 2 * Math.PI);
+    context.arc(x, yHorizon, RAYON, 0, 2 * Math.PI);
     context.stroke();
-    xHorizon += 250;
-  }
-  // Verticaux
-  let xVertical = 150;
-  var yVertical = 350;
-  for (var i = 1; i <= nbLigne; i++) {
+    // Cercle 1ère colonne
+    var y = DECALAGE_VERTICAL + 250 * i - 40;
     context.beginPath();
-    context.arc(xVertical, yVertical, 75, 0, 2 * Math.PI);
+    context.arc(xVertical, y, RAYON, 0, 2 * Math.PI);
     context.stroke();
-    yVertical += 250;
   }
 }
 
+var angle = 0;
 function cercles() {
   requestAnimationFrame(cercles);
   context.clearRect(0, 0, innerWidth, innerHeight);
   draw();
-  var x = 400;
-  var y = 350;
-  for (var i = 1; i <= nbCol; i++) {
+  for (var i = 0; i < NB_CERCLES; i++) {
+    // Point tournant sur les cercles de la ligne
+    var xCentreCercleLigne = DECALAGE_HORIZON + 250 * i;
+    var xLigne = xCentreCercleLigne + RAYON * Math.cos(angle * (i + 1));
+    var yLigne = RAYON * Math.sin(angle * (i + 1)) + 150;
     context.beginPath();
-    context.moveTo(x, 0);
-    context.lineTo(x, innerHeight);
+    context.arc(xLigne, yLigne, 5, 0, Math.PI * 2);
     context.stroke();
-    x += 250;
-  }
-  for (var i = 1; i <= nbLigne; i++) {
-    context.beginPath();
-    context.moveTo(0, y);
-    context.lineTo(innerWidth, y);
+    // Lignes bougeant par rapport aux cercles tournant sur la ligne
+    context.moveTo(xLigne, 0);
+    context.lineTo(xLigne, innerHeight);
     context.stroke();
-    y += 250;
-  }
 
+    // Point tournant sur les cercles de la colonne
+    var yCentreCercleColonne = DECALAGE_VERTICAL + 250 * i - 38;
+    var xColonne = DECALAGE_HORIZON - 250 + RAYON * Math.cos(angle * (i + 1));
+    var yColonne = yCentreCercleColonne + RAYON * Math.sin(angle * (i + 1));
+    context.beginPath();
+    context.arc(xColonne, yColonne, 5, 0, Math.PI * 2);
+    context.stroke();
+    // Lignes bougeant par rapport aux cercles tournant sur la ligne
+    context.moveTo(0, yColonne);
+    context.lineTo(innerWidth, yColonne);
+    context.stroke();
+  }
+  angle += 0.01;
+  if (angle > 2 * Math.PI + 0.5) {
+    angle = 0;
+  }
+}
+
+function courbes() {
+  requestAnimationFrame(courbes);
+  if (angle > 2 * Math.PI) {
+    alert("bonjour");
+  }
 }
